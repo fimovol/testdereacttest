f5669554709be3f212c2303424bc303f6ee4e3d7
fimovol <fimovolpe@gmail.com>
2022-04-02 18:32:23 -0500

    mas cambios

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,20 @@ import imagen from './img/a2.png'
 
 function App(){
   setTimeout( function() {
-    setestado({"border-right": "none"})
+    setestado({borderRight: "none"})
   },3000)
   const [estado,setestado] = useState({})
   return (
     <div className="App">
       <div className="centro">
-        <span className='efectoescritura' style={estado}>programador full stack</span>
-        <span className='efectoescrituraabajo'>en busqeda de su primer empleo</span>
+        <span className='efectoescritura' style={estado}>Programador full-stack</span>
+        <span className='efectoescrituraabajo'>en búsqueda de su primer empleo</span>
         <p className='parrafo'>sobre mi</p>
         <p className='parrafo'>contacto</p>
         <p className='parrafo'>mejores proyectos</p>
-        <p className='parrafo'>esto es nada que ver aqui</p>
+        <p className='parrafo'>esto es nada q</p>
     </div>
     <div className="contenedor">
-
         <div className="rojo">
             <img src={imagen} alt="" className="img"/>
         </div>
