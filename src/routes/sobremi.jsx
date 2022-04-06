c90ad3749196c1d0db56c43adc5bf1d6be2d93f1
fimovol <fimovolpe@gmail.com>
2022-04-06 11:50:11 -0500

    agregando el sobre mi

--- src/routes/sobremi.jsx
+++ src/routes/sobremi.jsx
@@ -1,5 +1,21 @@
+import styles from "../style/sobremi.module.css"
+import foto1 from "../img/foto1.png"
+
 export default function sobremi(){
     return (
-        <h1>te cuento quien soy y por que deberias contratarme</h1>
+        <>
+            <div className={styles.contenido} >
+                <p className={styles.p}>adasdadasjbd</p>
+                <p className={styles.p}>adasdadasjbd</p>
+                <p className={styles.p}>adasdadasjbd</p>
+                <p className={styles.p}>adasdadasjbd</p>
+                <p className={styles.p}>adasdadasjbd</p>
+                <p className={styles.p}>adasdadasjbd</p>
+                <div className={styles.block}> </div>
+            </div>
+            <div className={styles.cuadrado}>
+                <img src={foto1} alt="" className={styles.imagen} />
+            </div>
+        </>
     )
 }
