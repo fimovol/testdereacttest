c9c0246e84932669b19ade80d2fe08c73d968963
fimovol <fimovolpe@gmail.com>
2022-04-06 13:46:04 -0500

    quitando elementos de prueba

--- src/routes/sobremi.jsx
+++ src/routes/sobremi.jsx
@@ -1,20 +1,15 @@
 import styles from "../style/sobremi.module.css"
-import foto1 from "../img/foto1.png"
 
 export default function sobremi(){
     return (
         <>
-            <div className={styles.contenido} >
+            <div className={styles.contenido}>
                 <p className={styles.p}>adasdadasjbd</p>
                 <p className={styles.p}>adasdadasjbd</p>
                 <p className={styles.p}>adasdadasjbd</p>
                 <p className={styles.p}>adasdadasjbd</p>
                 <p className={styles.p}>adasdadasjbd</p>
                 <p className={styles.p}>adasdadasjbd</p>
-                <div className={styles.block}> </div>
-            </div>
-            <div className={styles.cuadrado}>
-                <img src={foto1} alt="" className={styles.imagen} />
             </div>
         </>
     )
