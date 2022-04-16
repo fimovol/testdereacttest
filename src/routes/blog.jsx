ab8825d0898eda6a3fc17a65a2c969c66a3b1682
fimovol <fimovolpe@gmail.com>
2022-04-16 11:15:16 -0500

    creando componentes para los blogs

--- src/routes/blog.jsx
+++ src/routes/blog.jsx
@@ -1,6 +1,7 @@
 import styles from "../style/sobremi.module.css"
 import styleblog from '../style/blog.module.css'
 import { Link,Outlet } from "react-router-dom"
+import imagen from '../img/movil.jpg'
 
 export default function blog(){
     
@@ -10,13 +11,24 @@ export default function blog(){
             <h1 className={styleblog.h1}>ten algunos articulos recomendados</h1>
             <section className={styleblog.blogcontainer}>
                 
+                   
+                 
+                
+                <div className={styleblog.caja}>
                     <Link to="jeje">
-                        <div className={styleblog.caja}> 
+                        <div className={styleblog.relleno}>
+                            <div className={styleblog.arriba}>
+                                <div className={styleblog.centrado}>
+                                    <img src={imagen} alt="asdadasd" className={styleblog.imagen}/>
+                                </div>
+                                
+                            </div>
+                            <div className={styleblog.abajo}></div>
                         </div>
                     </Link>
-                <div className={styleblog.caja}></div>
-                <div className={styleblog.caja}></div>
-                <div className={styleblog.caja}></div>
+                </div>
+                
+                
             </section>
         </div>
         
