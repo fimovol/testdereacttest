ca253647a66eacc6a345d1e6822b89db6f53ac23
fimovol <fimovolpe@gmail.com>
2022-05-11 09:57:48 -0500

    cambios

--- src/routes/mejoresproyectos.jsx
+++ src/routes/mejoresproyectos.jsx
@@ -5,6 +5,7 @@ import luces from "../img/luces.jpg"
 import movil from "../img/movil.jpg"
 import googlemeet from "../img/googlemeet.png"
 import scraping from "../img/scraping.jpg"
+import ecomerce from "../img/ecomerce.png"
 import {useTranslation} from "react-i18next"
 
 export default function Mejoresproyectos(){
@@ -14,12 +15,14 @@ export default function Mejoresproyectos(){
     const movilhref = 'https://github.com/fimovol/fpr_interfaces_senati'
     const googlemeethref = 'https://google-meet-clone-2000-dev.twil.io/index.html'
     const scrapinghref = 'https://djasncjknweiacbaslcjk.herokuapp.com/'
+    const ecomercehref = 'https://ecomercescraperjavier.herokuapp.com/'
     return (
         <div className={styles.contenedormejoresproyectos}>
             <h2 className={styles.h2}>{t("header.proyects")}</h2>
             <div className={styles.divproyectos}> 
                 <ProyectosQueRealice src={googlemeet} href={googlemeethref} alt="docker"/>
                 <ProyectosQueRealice src={scraping} href={scrapinghref} alt="scraping web"/> 
+                <ProyectosQueRealice src={ecomerce} href={ecomercehref} alt="ecomerce"/>
                 <ProyectosQueRealice src={luces} href={luceshref} alt="luces"/>
                 <ProyectosQueRealice src={movil} href={movilhref} alt="movil"/>
                 <ProyectosQueRealice src={calculadora} href={calculadorahref} alt="calculadora"/>
