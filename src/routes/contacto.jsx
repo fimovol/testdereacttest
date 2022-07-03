df311886e59acc9c6339f301201ff6213f0336a5
fimovol <fimovolpe@gmail.com>
2022-07-03 15:08:32 -0500

    listo

--- src/routes/contacto.jsx
+++ src/routes/contacto.jsx
@@ -8,7 +8,6 @@ export function Contacto(){
 
     return (
         <div className={styles.contenedorcontacto}>
-<<<<<<< HEAD
             <TituloDescriptivo>{t("header.contact")}</TituloDescriptivo>
             <h3 className="uppercase my-2" >{t("contact.mail")}:</h3>
             <p> 
@@ -28,15 +27,6 @@ export function Contacto(){
                     linkedin/javierfrancisco
                 </EnlaceContacto>
             </p>
-=======
-            <h2>{t("header.contact")}</h2>
-            <h3 style={estilos}>{t("contact.mail")}:</h3>
-            <p><a className={styles.psincapitalize} href="mailto:javiersanchezch@outlook.es">javiersanchezch@outlook.es</a> </p>
-            <h3 >GITHUB : </h3>
-            <p><a className={styles.psincapitalize} href="https://github.com/fimovol" target="_blank" rel="noreferrer">github.com/fimovol</a> </p>
-            <h3>LINKEDIN : </h3>
-            <p ><a className={styles.psincapitalize} href="https://www.linkedin.com/in/javier-francisco-sanchez" target="_blank" rel="noreferrer">linkedin/javierfrancisco</a> </p>
->>>>>>> 44f9ef7ca791531faa3d21253282e5881139c2fe
         </div>
         
     )
