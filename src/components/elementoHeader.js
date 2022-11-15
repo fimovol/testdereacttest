fa288069c9f91a1f4833d9f9eb670ceb044f60b6
fimovol <fimovolpe@gmail.com>
2022-11-14 21:20:43 -0500

    change " {`text`} " to " 'text' "

--- src/components/elementoHeader.js
+++ src/components/elementoHeader.js
@@ -1,11 +1,11 @@
 export const ElementoHeader = ({estilos,children,elemento,ruta}) => {
     return  <a href={`#${ruta}`}
-     className={`
+     className='
      font-sans capitalize text-morado hidden lg:block transition duration-200 select-none
      px-4 bg-celeste rounded-full mx-3 border-2 border-morado font-bold
      hover:border-celeste hover:text-celeste hover:bg-transparent
      
-     `}
+     '
      onClick={() => elemento(true)}
      style={estilos}
        >
